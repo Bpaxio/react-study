232b40f483c71c3970d028986346279052d9afe9
BPaxio <vrahlinskiy@cinimex.ru>
2020-02-03 12:50:05 +0300

    added Hooks to manage state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hello, React App</h1>
-  //   </div>
-  // );
-    return React.createElement('div',
-        {className: "App"},
-        React.createElement('h1', null, 'Hello, React App')
-    )
-}
+const App = () => {
+    const [persons, setPersons] = useState({
+        persons: [
+            {name: "Andy", age: 87},
+            {name: "Glok", age: 23},
+            {name: "Bill", age: 56}
+        ]
+    });
+    const [title, setTitle] = useState("Hello, React App");
+    const getNewState = () => {
+        setPersons({
+            persons: [
+                {name:"Andy", age:88},
+                {name:"Murai", age:24},
+                {name:"Bill", age:56}
+            ]
+        });
+    };
+    return (
+        <div className="App">
+            <h1>{title}</h1>
+            <button onClick={() => setTitle("New Title")}>Change Title</button>
+            <p>===================================</p>
+            <Person onCopy onCut onPaste name={persons.persons[0].name} age={persons.persons[0].age}/>
+            <Person name={persons.persons[1].name} age={persons.persons[1].age}/>
+            <Person name={persons.persons[2].name} age={persons.persons[2].age}>I don't want to say smth</Person>
+            <button onClick={getNewState}>Switch name</button>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const Person = (props) => {
+    const [age, updateAge] = useState(props.age);
+    return (
+        <div className="Person">
+            <p>Human "{props.name}" {age} years old</p>
+            <p>{props.children}</p>
+            <button onClick={() => updateAge(age + 1)}>Happy Birthday!!!</button>
+            <p>===================================</p>
+        </div>
+    )
+};
+
+export default Person;
