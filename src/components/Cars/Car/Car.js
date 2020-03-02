678f8b0f8bf64b5124a83a6b114cc08eeb8a91f9
BPaxio <vrahlinskiy@cinimex.ru>
2020-03-02 14:38:19 +0300

    added prop-types to set types of props in Component and example of warning situation in Car's component usage

--- src/components/Cars/Car/Car.js
+++ src/components/Cars/Car/Car.js
@@ -1,7 +1,11 @@
 import React, {useEffect} from 'react'
+import PropTypes from 'prop-types'
+
+
 import classes from "./Car.module.css";
 import Auxy from "../../../hoc/Auxy";
 import withClass from "../../../hoc/withClass";
+import Person from "../../Persons/Person/Person";
 
 const Car = (props) => {
     useEffect(
@@ -15,11 +19,18 @@ const Car = (props) => {
     console.log('[Car.js] rendering... ', props);
     return (
         <Auxy>
-                <p onClick={props.click}>"{props.mark}" milage: {props.mileage} miles</p>
-                <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.mark}/>
+                <p key="i1" onClick={props.click}>"{props.mark}" milage: {props.mileage} miles</p>
+                <p key="i2">{props.children}</p>
+            <input key="i3" type="text" onChange={props.changed} value={props.mark}/>
         </Auxy>
     )
+};
+
+Car.propTypes = {
+    click: PropTypes.func,
+    mark: PropTypes.string,
+    mileage: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Car, classes.Car);
